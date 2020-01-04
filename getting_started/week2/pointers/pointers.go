60581d2e44c054f7da792d3fc21cf005dabda2a8
skad00sh <heramblonkar@gmail.com>
2020-01-04 14:06:30 +0530

    Added pointers from week 2 getting started

---
+++ getting_started/week2/pointers/pointers.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+func  main()  {
+  var x int = 1
+  var y int
+  var ip *int
+  fmt.Println("Value of x is: ",x)
+  fmt.Println("Value of y is :", y)
+  fmt.Println("Data at memory address of int is: ", ip)
+  ip = &x
+  y = *ip
+  fmt.Println("Memory address(pointer) of x is: ", ip)
+  fmt.Println("Data at memory address of x is: ", y)
+  a := x
+  fmt.Println("Memory address of a is: ", &a)
+  a_pointer := &a
+  fmt.Println("Data at memory address of a is: ", *a_pointer)
+}
+
+//Output
+/*
+Value of x is:  1
+Value of y is : 0
+Data at memory address of int is:  <nil>
+Memory address(pointer) of x is:  0xc000096000
+Data at memory address of x is:  1
+Memory address of a is:  0xc000096028
+Data at memory address of a is:  1
+*/
